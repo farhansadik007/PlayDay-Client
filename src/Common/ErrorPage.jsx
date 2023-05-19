f7f9d7d4e1ba351e263b6439b56a6428276c2d62
farhansadik007 <sadik.zisan@gmail.com>
2023-05-19 13:03:42 +0600

    error page done

--- src/Common/ErrorPage.jsx
+++ src/Common/ErrorPage.jsx
@@ -4,6 +4,7 @@ const ErrorPage = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <h1 className="lg:text-[300px] sm:text-9xl text-9xl text-red-500">404</h1>
+            <img src="https://i.gifer.com/5LSi.gif" alt="" />
             <h2 className="lg:text-[100px] sm:text-5xl text-5xl">Not Found!</h2>
             <Link className="btn btn-success mt-12 text-white" to='/' >Back to Home</Link>
             </div>
